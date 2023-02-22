32160fe569600258901319672fb2fee2b30602b9
Hafimuthasir <muthasirhafi@gmail.com>
2023-02-22 12:14:37 +0530

    m

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pl9',
-        'USER':'postgres',
+        'USER':'connectuser',
         'PASSWORD':'admin',
         'HOST':'localhost'
     }
