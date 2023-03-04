c8942246bf276adb5fde0e601c0a254d002d1318
Hafimuthasir <muthasirhafi@gmail.com>
2023-03-04 21:37:52 +0530

    m

--- djangoproject/asgi.py
+++ djangoproject/asgi.py
@@ -6,8 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-
+import django
 import os
+
+django.setup()
 
 from django.core.asgi import get_asgi_application
 
